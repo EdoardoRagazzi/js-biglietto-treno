4506d7bfc75b4c1176cff9031aca5df2c13b38ea
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-05-27 15:08:54 +0200

    variabili / prezzi

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,24 @@
-var kilometri = prompt('Travel Km');
-var eta = prompt('Età traveler')
+var kilometri = parseInt(prompt('Travel Km'));
+var eta = parseInt(prompt('Età traveler'));
+
+console.log(kilometri);
+console.log(eta);
+
+var prezzototale = kilometri * 0.21;
+
+var sconto = 0;
+
+if (eta < 18) {
+    sconto = prezzototale * 0.2;
+} else if (eta >= 65) {
+    sconto = prezzototale * 0.4;
+}
+console.log(sconto);
+
+var prezzofinale = prezzototale - sconto;
+console.log(prezzofinale);
+
+
+
+
+
